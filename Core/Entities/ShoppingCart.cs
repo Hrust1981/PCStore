f6a49ebccf7521a036c9fc5928f90843c60e7796
Hrust1981 <rustamshar1981@gmail.com>
2024-06-20 00:43:41 +0500

    Created a shoppingcart entity

---
+++ Core/Entities/ShoppingCart.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Entities
+{
+    public class ShoppingCart : Entity
+    {
+        public ShoppingCart(string name) : base(name)
+        {
+        }
+    }
+}
--- Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e510d6aa7147510eb271b41f8a5bb54beacff98")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad5a9afeaa88401b8ebd4d5904ac9d254a162168")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
